b00c6df2fe066be2f5e80c2fce23bcbfa06abb8f
ttonosak <ttonosak@fsi.co.jp>
2019-02-09 19:37:30 +0900

    4.1 確認画面を全チェック＆確認に 月締め画面に全チェックを追加 ライブラリを更新

--- src/js/check.js
+++ src/js/check.js
@@ -1,91 +1,122 @@
-$(document).ready(function(){
-  // COMPANYにチェックをつけるボタン追加
-	if ($("#DCMLTAPRV")[0]) {
-    $("div.globalnavi")
-      .append("<input type='button' value='チェックを付ける' class='check-button'></input>")
-      .bind("click", function(e) {
-        $("[id^=BTNDCCOR]").each(function(){
-          var tmp = $(this).attr("id").substr(8);
-          tmp = tmp.substr(0, tmp.length - 1);
-          $("#CHK" + tmp).prop("checked",true);
+// COMPANYのURL
+var companyURLreg = /.*\/cws\/cws/
+// 社員名簿DBのURL
+var meiboURLreg = /.*_office-only\/meiboDB\/.*/
+// 簡易WF URL
+var wfURLreg = /.*\/wf\/.*/
+
+$(document).ready(function () {
+  var href = window.location.href;
+  // ---------------------------------------
+  // COMPANY
+  // ---------------------------------------
+  if (href.match(companyURLreg)) {
+    // 実績確認画面にチェック＆確認をつけるボタン
+    if ($("#DCMLTAPRV")[0]) {
+      $("div.globalnavi")
+        .append("<input type='button' value='チェック＆確認' class='check-button'></input>")
+        .bind("click", function (e) {
+          $("[id^=BTNDCCOR]").each(function () {
+            var tmp = $(this).attr("id").substr(8);
+            tmp = tmp.substr(0, tmp.length - 1);
+            $("#CHK" + tmp).prop("checked", true);
+          });
+          $("#DCMLTAPRV")[0].click();
         });
+    }
+    // 月締承認画面にチェック＆確認をつけるボタン
+    if ($("#MLTSBMTAPRV")[0]) {
+      $("div.globalnavi")
+        .append("<input type='button' value='すべてチェック' class='check-button'></input>")
+        .bind("click", function (e) {
+          $("[id^=cbx]").each(function () {
+            $(this).prop("checked", true);
+          });
+        });
+    }
+
+    // COMPANYにCOPY／PASTE／全部ボタンを追加
+    if ($("#KNMTMRNGSTD")[0]) {
+      $('html').append("<input type='hidden' id='jviuh5o6y6622oHIDDEN'>");
+      addCpPasteButtons();
+
+      // DOM要素の変更を監視してコピペボタンの追加をする
+      var observer = new MutationObserver(function (MutationRecords, MutationObserver) {
+        addCpPasteButtons();
       });
+      observer.observe($('#PM_PANEL_ENTRY_TIME_WIDGET_CONTAINER_AREA').get(0), {
+        childList: true,
+      });
+    }
+    return;
   }
 
-  // 簡易WFの選択肢を絞る
-  var href = window.location.href;
-  var sels = $("select");
-  if (sels[0]) {
-    var reg1 = /^開発計画書（/
-    var reg2 = /^納入物件チェックリスト/
-    chrome.storage.local.get(function(items) {
+  // ---------------------------------------
+  // 社員DB
+  // ---------------------------------------
+  if (href.match(meiboURLreg)) {
+    // 右クリックを有効化
+    $("html").attr('oncontextmenu', 'return true');
+    return;
+  }
+
+  // ---------------------------------------
+  // 簡易WF
+  // ---------------------------------------
+  if (href.match(wfURLreg)) {
+    // 選択肢を絞る
+    var sels = $("select");
+    if (sels[0]) {
+      var reg1 = /^開発計画書（/
+      var reg2 = /^納入物件チェックリスト/
+      chrome.storage.local.get(function (items) {
         var bushoCode = (items.bushoCode) ? items.bushoCode : "498";
         var reg3 = new RegExp(bushoCode);
         var addButton = (void 0);
-        for(var i=0; i< sels.length; i++) {
+        for (var i = 0; i < sels.length; i++) {
           var options = $(sels[i]).children();
-          for(var j=0; j<options.length; j++) {
+          for (var j = 0; j < options.length; j++) {
             var curStr = $(options[j]).text();
-            if (curStr.match(reg1) || curStr.match(reg2)){
-              if(!curStr.match(reg3)) {
+            if (curStr.match(reg1) || curStr.match(reg2)) {
+              if (!curStr.match(reg3)) {
                 $(options[j]).hide();
                 addButton = sels[i];
               }
             }
           }
         }
-        if (addButton){
+        if (addButton) {
           // ボタンを追加。IDは重複しないように乱数にしてある
           $(addButton).parent().append("<input type='button' value='全表示' title='隠した部所を全表示' id='jviuh5o6y6622osz'></input>");
-          $(document).on("click", "#jviuh5o6y6622osz", function(e){
+          $(document).on("click", "#jviuh5o6y6622osz", function (e) {
             var options = $(addButton).children();
-            for(var j=0; j<options.length; j++) {
+            for (var j = 0; j < options.length; j++) {
               var curStr = $(options[j]).show();
             }
           });
         }
-    });
+      });
+    }
+    return;
   }
-
-  // 社員DBで右クリックを有効化
-  var meiboURLreg = /.*_office-only\/meiboDB\/.*/
-  if (href.match(meiboURLreg)){
-    $("html").attr('oncontextmenu','return true');
-    //$("div[style*='background:url(']")
-  }
-  
-  // COMPANYにCOPY／PASTE／全部ボタンを追加
-  if ($("#KNMTMRNGSTD")[0]){
-    $('html').append("<input type='hidden' id='jviuh5o6y6622oHIDDEN'>");
-    addCpPasteButtons();
-    
-    // DOM要素の変更を監視してコピペボタンの追加をする
-    var observer = new MutationObserver(function (MutationRecords, MutationObserver) {
-      addCpPasteButtons();
-    });
-    observer.observe($('#PM_PANEL_ENTRY_TIME_WIDGET_CONTAINER_AREA').get(0), {
-      childList: true,
-    });
-  }
-  
 });
 
 /**
  * COMPNAYの日毎入力画面にコピペボタンを追加する
  */
 function addCpPasteButtons() {
-  $(".PmPanelEntryTimeStateEachRowStyle").each(function(index){
-    $(this).append("<button style='margin-left:10px' id='jviuh5o6y6622oXY-"+index+"' title='実績時間のコピー'>Ｃ</button>");
-    $(this).append("<button id='jviuh5o6y6622oPASTE-"+index+"' title='実績時間の貼り付け'>Ｐ</button>");
-    $(this).append("<button style='margin-left:10px' id='jviuh5o6y6622oJITSU-"+index+"' title='本日の稼働時間を入力'>全</button>");
-    $(document).on("click", "#jviuh5o6y6622oXY-"+index, function(e){
+  $(".PmPanelEntryTimeStateEachRowStyle").each(function (index) {
+    $(this).append("<button style='margin-left:10px' id='jviuh5o6y6622oXY-" + index + "' title='実績時間のコピー'>Ｃ</button>");
+    $(this).append("<button id='jviuh5o6y6622oPASTE-" + index + "' title='実績時間の貼り付け'>Ｐ</button>");
+    $(this).append("<button style='margin-left:10px' id='jviuh5o6y6622oJITSU-" + index + "' title='本日の実働時間を入力'>全</button>");
+    $(document).on("click", "#jviuh5o6y6622oXY-" + index, function (e) {
       var val = {
         hour: $('#PmDdEntryTimeInputWidget_' + index + 'H').val(),
         minute: $('#PmDdEntryTimeInputWidget_' + index + 'M').val()
       }
       $('#jviuh5o6y6622oHIDDEN').val(JSON.stringify(val));
     });
-    $(document).on("click", "#jviuh5o6y6622oPASTE-"+index, function(e){
+    $(document).on("click", "#jviuh5o6y6622oPASTE-" + index, function (e) {
       var val = $('#jviuh5o6y6622oHIDDEN').val();
       if (val && 0 < val.length) {
         var o = JSON.parse(val);
@@ -93,7 +124,7 @@ function addCpPasteButtons() {
         $('#PmDdEntryTimeInputWidget_' + index + 'M').val(o.minute);
       }
     });
-    $(document).on("click", "#jviuh5o6y6622oJITSU-"+index, function(e){
+    $(document).on("click", "#jviuh5o6y6622oJITSU-" + index, function (e) {
       var hour = $('#JTDMIH').text();
       var minute = $('#JTDMIM').text();
       if (hour && minute) {
